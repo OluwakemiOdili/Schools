93368756bb36e13140ada5c6cded15b9875d6dc0
Support <Support@WEMA-TRAINING-S>
2022-08-26 00:25:42 +0100

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schools.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int studentId { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }   
+        public ICollection<Student> StudentDetails { get; set; }
+
+    }
+
+    public class Studentdetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int studentID { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public int StudentID { get; set; }
+
+        [ForeignKey("StudentsID")]
+        public Student student { get; set; }    
+
+
+    }
+}
---
+++ Studentdb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schools.Models
+
+{
+    public class Studentdb
+    {
+        public class StudentContext : DbContext
+        {
+            public StudentContext(DbContextOptions options) : base(options)
+            {
+
+            }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer("Server=.;Database=Bankappdb;Trusted_Connection=True;");
+                }
+            }
+            public DbSet<Student> Student { get; set; }
+            public DbSet<Studentdb> Studentdb { get; set; }
+           
+        }
+    }
+}
